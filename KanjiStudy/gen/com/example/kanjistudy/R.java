33301d341c6a95d9210c55fad282aff465c6d223
Maryssa <mecrews@smu.edu>
2014-05-07 00:31:50 -0500

    set up main page

--- KanjiStudy/gen/com/example/kanjistudy/R.java
+++ KanjiStudy/gen/com/example/kanjistudy/R.java
@@ -1077,9 +1077,13 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int button=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
+        public static final int icon=0x7f020059;
+        public static final int logo=0x7f02005a;
     }
     public static final class id {
+        public static final int aboutUs=0x7f05003d;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,10 +1095,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050040;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int chapter_select=0x7f05003f;
+        public static final int chapters_button=0x7f05003e;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1247,6 +1253,7 @@ containing a value of this type.
         public static final int chapter_select=0x7f0a0010;
         public static final int hello_world=0x7f0a000e;
         public static final int kanji=0x7f0a0011;
+        public static final int main_menu=0x7f0a0013;
         public static final int vocabulary=0x7f0a0012;
     }
     public static final class style {
